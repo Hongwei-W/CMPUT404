d972fe25e8b24c0d58a205200f7a9dcf04dbafdf
Hongwei-W <hongwei_charles_wang@outlook.com>
2022-01-12 18:30:48 -0700

    *NEW [print source code] add function that print source code

--- lab.py
+++ lab.py
@@ -4,3 +4,6 @@ print(rq.__version__)
 
 google = rq.get("http://www.google.com")
 print(google)
+
+source_code = rq.get("https://raw.githubusercontent.com/Hongwei-W/CMPUT404/master/lab.py")
+print(source_code.content)
